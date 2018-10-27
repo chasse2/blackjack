08bf0f5e8990451bdc74001a721f6c1f50d3a36f
chasse2 <eenglish43@yahoo.com>
2018-10-27 07:17:33 -0400

    Minor modifications

---
+++ src/main/java/spec/HandSpecs.java
@@ -0,0 +1,12 @@
+package spec;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class HandSpecs {
+    final Set<HandSpec> all() {
+        final Set<HandSpec> specs = new LinkedHashSet<>();
+        //todo - fill
+        return specs;
+    }
+}
